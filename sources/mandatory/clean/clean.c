d5541dd0c8603a4c61e28cd115c7bce48b93406f
Thea Wang <theawang@yahoo.com>
2023-08-21 11:44:28 +0200

    revert before shit change

--- sources/mandatory/clean/clean.c
+++ sources/mandatory/clean/clean.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 11:43:55 by twang             #+#    #+#             */
-/*   Updated: 2023/08/21 11:17:36 by twang            ###   ########.fr       */
+/*   Updated: 2023/08/21 11:44:01 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,6 @@ void	clean_textures(t_game *g)
 	}
 }
 
-void	clean_colors(t_game *g)
-{
-	int	i;
-
-	i = 0;
-	while (i < 2)
-	{
-		if (i < 2 && (g->color[i].color))
-			// free((void *)g->color[i].color);
-		i++;
-	}
-}
-
 void	clean_map(t_game *g)
 {
 	free_split(g->map.map, g->map.size.y);
@@ -59,9 +46,10 @@ int	clean(t_game *g)
 	if (g->map.map)
 		clean_map(g);
 	clean_textures(g);
-	clean_colors(g);
-	mlx_destroy_window(g->mlx, g->window);
-	mlx_destroy_display(g->mlx);
+	if (g->window)
+		mlx_destroy_window(g->mlx, g->window);
+	if (g->mlx)
+		mlx_destroy_display(g->mlx);
 	free(g->mlx);
 	exit(0);
 }
