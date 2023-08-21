14eabe079bb6189ca6027d64d19b4a7e08800844
Thea Wang <theawang@yahoo.com>
2023-08-21 11:35:30 +0200

    add mlx_linux

--- sources/mandatory/clean/clean.c
+++ sources/mandatory/clean/clean.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
+/*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 11:43:55 by twang             #+#    #+#             */
-/*   Updated: 2023/08/07 12:02:56 by wangthea         ###   ########.fr       */
+/*   Updated: 2023/08/21 11:17:36 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,19 @@ void	clean_textures(t_game *g)
 	}
 }
 
+void	clean_colors(t_game *g)
+{
+	int	i;
+
+	i = 0;
+	while (i < 2)
+	{
+		if (i < 2 && (g->color[i].color))
+			// free((void *)g->color[i].color);
+		i++;
+	}
+}
+
 void	clean_map(t_game *g)
 {
 	free_split(g->map.map, g->map.size.y);
@@ -46,10 +59,9 @@ int	clean(t_game *g)
 	if (g->map.map)
 		clean_map(g);
 	clean_textures(g);
-	if (g->window)
-		mlx_destroy_window(g->mlx, g->window);
-	// if (g->mlx)
-		// mlx_destroy_display(g->mlx);
+	clean_colors(g);
+	mlx_destroy_window(g->mlx, g->window);
+	mlx_destroy_display(g->mlx);
 	free(g->mlx);
 	exit(0);
 }
