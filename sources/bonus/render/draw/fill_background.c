883fdfe6f6a30e3994e1088bebf837230e48f636
Arthur <arthurascedusnkrs@gmail.com>
2023-08-23 08:39:17 +0200

    Mini map with border and grid

--- sources/bonus/render/draw/fill_background.c
+++ sources/bonus/render/draw/fill_background.c
@@ -12,27 +12,6 @@
 
 #include "cub3D_thea.h"
 
-void	draw_frame(t_game *g, int size_x, int size_y, int color)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	while (i < size_x)
-	{
-		j = 0;
-		while (j < size_y)
-		{
-			if (i % 30 == 0 || j % 30 == 0)
-				my_mlx_pixel_put(&g->draw, i, j, H_PINK);
-			if (i < 5 || j < 5 || j > size_y - 5 || i > size_x - 5)
-				my_mlx_pixel_put(&g->draw, i, j, color);
-			j++;
-		}
-		i++;
-	}
-}
-
 void	fill_background(t_game *g, int size_x, int size_y, int color)
 {
 	int	i;
@@ -48,21 +27,5 @@ void	fill_background(t_game *g, int size_x, int size_y, int color)
 			j++;
 		}
 		i++;
-	}
-}
-
-void	init_map(t_game *g, int x, int y)
-{
-	int	i;
-
-	i = x;
-	printf("x=%d et y=%d\nmap: %c\n", x, y, g->map.map[y][i]);
-	while (i > x - 5 && i >= 0)
-	{
-		if (g->map.map[i][y] == wall)
-			draw_square(g, 140 - (27 * (x - i)), 140, H_RED);
-		if (g->map.map[i][y] == space)
-			draw_square(g, 140 - (27 * (x - i)), 140, H_WHITE);
-		i--;
 	}
 }
