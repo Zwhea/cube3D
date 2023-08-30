bd1d414cb8262816114cf9ee809b5198fb77434a
Thea Wang <theawang@yahoo.com>
2023-08-30 09:32:00 +0200

    correction: parsing for bonus texture / add: start function draw circle - not working yet

--- sources/bonus/parsing/assets/textures/bonus_textures.c
+++ sources/bonus/parsing/assets/textures/bonus_textures.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:02:23 by twang             #+#    #+#             */
-/*   Updated: 2023/08/28 16:23:39 by twang            ###   ########.fr       */
+/*   Updated: 2023/08/30 09:31:02 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	handle_ceiling_texture(t_game *g, char *line)
 
 	i = 0;
 	if (g->textures.bonus == false)
-		return ;
+		error_switchman(g, wrong_texture);
 	if (g->textures.walls[ceiling_texture].sprite)
 		error_switchman(g, wrong_textures);
 	while (line[i] && ft_strchr(TEXTURE_CEILING, line[i]))
@@ -42,7 +42,7 @@ void	handle_floor_texture(t_game *g, char *line)
 
 	i = 0;
 	if (g->textures.bonus == false)
-		return ;
+		error_switchman(g, wrong_texture);
 	if (g->textures.walls[floor_texture].sprite)
 		error_switchman(g, wrong_textures);
 	while (line[i] && ft_strchr(TEXTURE_FLOOR, line[i]))
