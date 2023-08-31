a5582bd8e613f7c267895c0667a8142edc1dd2d7
Zwhea <theawang@yahoo.com>
2023-08-31 14:29:31 +0200

    fix: made it pritty

--- sources/bonus/parsing/map/map_checker.c
+++ sources/bonus/parsing/map/map_checker.c
@@ -6,7 +6,7 @@
 /*   By: wangthea <wangthea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/05 12:19:40 by twang             #+#    #+#             */
-/*   Updated: 2023/08/31 14:25:57 by wangthea         ###   ########.fr       */
+/*   Updated: 2023/08/31 14:28:53 by wangthea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 /*---- prototypes ------------------------------------------------------------*/
 
-static void	_backtracking(t_game *g, int x, int y);
 static void	_check_char(t_game *g);
 static void	_set_player(t_game *g, int i, int j);
 static void	_check_door(t_game *g, int i, int j);
+static void	_backtracking(t_game *g, int x, int y);
 
 /*----------------------------------------------------------------------------*/
 
@@ -30,6 +30,46 @@ void	map_checker(t_game *g)
 	_backtracking(g, g->player.pos.y, g->player.pos.x);
 	if (g->map.b_map)
 		free_split(g->map.b_map, g->map.size.y);
+}
+
+static void	_check_char(t_game *g)
+{
+	int	i;
+	int	j;
+
+	i = -1;
+	while (g->map.map[++i])
+	{
+		if (ft_isempty(g->map.map[i]))
+			error_switchman(g, wrong_map);
+		set_map_width(g, g->map.map[i]);
+		j = -1;
+		while (g->map.map[i][++j])
+		{
+			if (!ft_strchr(MAP_CHARSET, g->map.map[i][j]))
+				error_switchman(g, wrong_char);
+			if (ft_strchr(PLAYER_CHARSET, g->map.map[i][j]))
+				_set_player(g, i, j);
+			if (g->map.map[i][j] == door)
+				_check_door(g, i, j);
+		}
+	}
+}
+
+static void	_set_player(t_game *g, int i, int j)
+{
+	g->player.player++;
+	set_direction(g, g->map.map[i][j]);
+	set_vector(&g->player.pos, j, i);
+	set_vector_f(&g->player.posf, j + 0.5, i + 0.5);
+	set_vector_f(&g->player.start, j + 0.5, i + 0.5);
+}
+
+static void	_check_door(t_game *g, int i, int j)
+{
+	if (g->map.map[i][j + 1] != wall || g->map.map[i][j - 1] != wall)
+		if (g->map.map[i + 1][j] != wall || g->map.map[i - 1][j] != wall)
+			error_switchman(g, wrong_door);
 }
 
 static void	_backtracking(t_game *g, int x, int y)
@@ -56,43 +96,4 @@ static void	_backtracking(t_game *g, int x, int y)
 		_backtracking(g, x - 1, y);
 	}
 	g->map.b_map[x][y] = wall;
-}
-
-static void	_check_char(t_game *g)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (g->map.map[++i])
-	{
-		if (ft_isempty(g->map.map[i]))
-			error_switchman(g, wrong_map);
-		set_map_width(g, g->map.map[i]);
-		j = -1;
-		while (g->map.map[i][++j])
-		{
-			if (!ft_strchr(MAP_CHARSET, g->map.map[i][j]))
-				error_switchman(g, wrong_char);
-			if (ft_strchr(PLAYER_CHARSET, g->map.map[i][j]))
-				_set_player(g, i, j);
-			if (g->map.map[i][j] == door)
-				_check_door(g, i, j);
-		}
-	}
-}
-static void	_set_player(t_game *g, int i, int j)
-{
-	g->player.player++;
-	set_direction(g, g->map.map[i][j]);
-	set_vector(&g->player.pos, j, i);
-	set_vector_f(&g->player.posf, j + 0.5, i + 0.5);
-	set_vector_f(&g->player.start, j + 0.5, i + 0.5);
-}
-
-static void	_check_door(t_game *g, int i, int j)
-{
-	if (g->map.map[i][j + 1] != wall || g->map.map[i][j - 1] != wall)
-		if (g->map.map[i + 1][j] != wall || g->map.map[i - 1][j] != wall)
-			error_switchman(g, wrong_door);
 }
