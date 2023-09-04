540e30ff803d3d732d600b079be4320f835d7af0
Thea Wang <theawang@yahoo.com>
2023-09-04 13:29:12 +0200

    modif: key switchman

--- sources/bonus/init/mlx/key_switchman.c
+++ sources/bonus/init/mlx/key_switchman.c
@@ -6,11 +6,70 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 09:29:37 by twang             #+#    #+#             */
-/*   Updated: 2023/09/04 09:36:13 by twang            ###   ########.fr       */
+/*   Updated: 2023/09/04 13:28:09 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3D_thea.h"
+
+/*---- prototypes ------------------------------------------------------------*/
+
+static int _view_switch(t_keycode key, t_game *g);
+
+/*----------------------------------------------------------------------------*/
+
+int	key_press(t_keycode key, t_game *g)
+{
+	if (key == esc_key)
+		clean(g);
+	if (key == l_key)
+		legend_init(g);
+	return (0);
+}
+
+int key_switch(t_keycode key, t_game *g)
+{
+	if (key == w_key)
+		w_move(g);
+	if (key == a_key)
+		a_move(g);
+	if (key == s_key)
+		s_move(g);
+	if (key == d_key)
+		d_move(g);
+	if (key == up_key)
+		w_move(g);
+	if (key == down_key)
+		s_move(g);
+	if (key == left_key || key == right_key)
+		_view_switch(key, g);
+	return (0);
+}
+
+static int _view_switch(t_keycode key, t_game *g)
+{
+	if (g->player.diff_pov != 1 && g->player.last_pov > 0)
+		g->player.diff_pov = 1;
+	if (key == left_key)
+		view_left(g);
+	if (key == right_key)
+		view_right(g);
+	return (0);
+}
+
+int	mouse_click(t_keycode key, t_game *g)
+{
+	int					i;
+	const t_lst_react	react_tab[] = {{m_left_key, &open_door}};
+
+	i = -1;
+	while (++i < 1)
+	{
+		if (key == react_tab[i].key)
+			return (react_tab[i].func(g));
+	}
+	return (0);
+}
 
 int	mouse_move(int x, int y, t_game *g)
 {
@@ -26,44 +85,5 @@ int	mouse_move(int x, int y, t_game *g)
 		view_right(g);
 	}
 	g->player.last_pov = x;
-	return (0);
-}
-
-int	key_press(t_keycode key, t_game *g)
-{
-	int					i;
-	const t_lst_react	react_tab[] = {{esc_key, &clean}, \
-										{l_key, &legend_init}, \
-										{w_key, &w_move}, \
-										{a_key, &a_move}, \
-										{s_key, &s_move}, \
-										{d_key, &d_move}, \
-										{up_key, &w_move}, \
-										{down_key, &s_move}, \
-										{left_key, &view_left}, \
-										{right_key, &view_right}};
-
-	i = -1;
-	if (g->player.diff_pov != 1 && g->player.last_pov > 0)
-		g->player.diff_pov = 1;
-	while (++i < 11)
-	{
-		if (key == react_tab[i].key)
-			return (react_tab[i].func(g));
-	}
-	return (0);
-}
-
-int	key_click(t_keycode key, t_game *g)
-{
-	int					i;
-	const t_lst_react	reacti_tab[] = {{m_left_key, &open_door}};
-
-	i = -1;
-	while (++i < 1)
-	{
-		if (key == reacti_tab[i].key)
-			return (reacti_tab[i].func(g));
-	}
 	return (0);
 }
