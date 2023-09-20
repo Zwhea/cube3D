20f4f2813fccc02bf701684c50146197415d230b
Thea Wang <theawang@yahoo.com>
2023-09-20 10:29:33 +0200

    add: check of the fucking doors (north & south are reversed?) ON DIT MERCI HENRIIIIIIIIIII

--- sources/bonus/motions/doors.c
+++ sources/bonus/motions/doors.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 09:39:28 by twang             #+#    #+#             */
-/*   Updated: 2023/09/08 14:39:18 by twang            ###   ########.fr       */
+/*   Updated: 2023/09/20 10:28:18 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,42 @@
 
 /*---- prototypes ------------------------------------------------------------*/
 
-static void	_check_door(t_game *g, int y, int x);
+static int		_check_direction(double angle);
+static void		_check_door(t_game *g, int y, int x);
 
 /*----------------------------------------------------------------------------*/
 
 int	open_door(t_game *g)
 {
-	//si je regarde en haut:
-	_check_door(g, g->player.pos.y - 1, g->player.pos.x);
-	//si je regarde en bas:
-	_check_door(g, g->player.pos.y + 1, g->player.pos.x);
-	//si je regarde a gauche:
-	_check_door(g, g->player.pos.y, g->player.pos.x - 1);
-	//si je regarde a droite:
-	_check_door(g, g->player.pos.y, g->player.pos.x + 1);
+	if (_check_direction(g->player.angle_view) == 0)
+		_check_door(g, g->player.pos.y - 1, g->player.pos.x);
+	if (_check_direction(g->player.angle_view) == 1)
+		_check_door(g, g->player.pos.y + 1, g->player.pos.x);
+	if (_check_direction(g->player.angle_view) == 2)
+		_check_door(g, g->player.pos.y, g->player.pos.x - 1);
+	if (_check_direction(g->player.angle_view) == 3)
+		_check_door(g, g->player.pos.y, g->player.pos.x + 1);
 	return (0);
+}
+
+static int	_check_direction(double angle)
+{
+	double	pov;
+
+	pov = remainder(angle, (2 * M_PI));
+	if ((pov > 5 * M_PI / 4 && pov < 7 * M_PI / 4) \
+			|| (pov > -3 * M_PI / 4 && pov < -M_PI_4))
+		return (0);
+	if ((pov > M_PI_4 && pov < 3 * M_PI / 4) \
+			|| (pov > -7 * M_PI / 4 && pov < -5 * M_PI / 4))
+		return (1);
+	if ((pov > 3 * M_PI / 4 && pov < 5 * M_PI / 4) \
+			|| (pov > -5 * M_PI / 4 && pov < -3 * M_PI / 4))
+		return (2);
+	if ((pov > -M_PI_4 && pov < M_PI_4) \
+			|| (pov > -7 * M_PI / 4 && pov < -2 * M_PI))
+		return (3);
+	return (-1);
 }
 
 static void	_check_door(t_game *g, int y, int x)
