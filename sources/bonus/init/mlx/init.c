8aa29a19acfae2a3f0e8daa94abf7a0b3107b080
Thea Wang <theawang@yahoo.com>
2023-09-20 16:12:48 +0200

    add: new keys management

--- sources/bonus/init/mlx/init.c
+++ sources/bonus/init/mlx/init.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 19:38:50 by twang             #+#    #+#             */
-/*   Updated: 2023/09/20 16:07:57 by twang            ###   ########.fr       */
+/*   Updated: 2023/09/20 16:09:34 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,6 @@ void	init_mlx_functions(t_game *g)
 	mlx_hook(g->window, 4, 1L << 2, mouse_switch, g);
 	mlx_hook(g->window, 5, 1L << 3, mouse_release, g);
 	mlx_hook(g->window, 6, 1L << 6, mouse_move, g);
-	// mlx_key_hook(g->window, key_press, g);
 	mlx_loop_hook(g->mlx, _key_manager, g);
 	mlx_hook(g->window, 17, 1L << 17, clean, g);
 	mlx_loop(g->mlx);
