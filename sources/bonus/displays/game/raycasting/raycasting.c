f40302f2dd0a5f364b9e745df6c2ec306ca841d0
arthur <arthurascedusnkrs@gmail.com>
2023-09-27 15:56:33 +0200

    NORM + fix fisheye

--- sources/bonus/displays/game/raycasting/raycasting.c
+++ sources/bonus/displays/game/raycasting/raycasting.c
@@ -13,72 +13,67 @@
 #include "cub3D_thea.h"
 #include "cub3D_arthur.h"
 
+void	init_var(t_game *g, float angle)
+{
+
+}
+
 void	raycasting(t_game *g, float angle)
 {
-	t_vector_f	ray_start;
-	t_vector_f	ray_dir;
-	t_vector_f	ray_unit;
-	t_vector_f	ray_len;
-	t_vector	check;
-	t_vector	step;
-	int			wall;
-	int			door;
-	double		dist;
-	t_vector_f	intersection;
-
-	ray_unit.x = sqrt(1 + pow(tan(angle), 2));
-	ray_unit.y = sqrt(1 + pow(1 / tan(angle), 2));
-	ray_dir.x = cos(angle);
-	ray_dir.y = sin(angle);
-	ray_start = g->player.posf;
-	check = g->player.pos;
-	if (ray_dir.x < 0)
+	g->ray.ray_unit.x = sqrt(1 + pow(tan(angle), 2));
+	g->ray.ray_unit.y = sqrt(1 + pow(1 / tan(angle), 2));
+	g->ray.ray_dir.x = cos(angle);
+	g->ray.ray_dir.y = sin(angle);
+	g->ray.ray_start = g->player.posf;
+	g->ray.check = g->player.pos;
+	if (g->ray.ray_dir.x < 0)
 	{
-		step.x = -1;
-		ray_len.x = (ray_start.x - (float)check.x) * ray_unit.x;
+		g->ray.step.x = -1;
+		g->ray.ray_len.x = (g->ray.ray_start.x - (float)g->ray.check.x) * g->ray.ray_unit.x;
 	}
 	else
 	{
-		step.x = 1;
-		ray_len.x = ((float)(check.x + 1) - ray_start.x) * ray_unit.x;
+		g->ray.step.x = 1;
+		g->ray.ray_len.x = ((float)(g->ray.check.x + 1) - g->ray.ray_start.x) * g->ray.ray_unit.x;
 	}
-	if (ray_dir.y < 0)
+	if (g->ray.ray_dir.y < 0)
 	{
-		step.y = -1;
-		ray_len.y = (ray_start.y - (float)check.y) * ray_unit.y;
+		g->ray.step.y = -1;
+		g->ray.ray_len.y = (g->ray.ray_start.y - (float)g->ray.check.y) * g->ray.ray_unit.y;
 	}
 	else
 	{
-		step.y = 1;
-		ray_len.y = ((float)(check.y + 1) - ray_start.y) * ray_unit.y;
+		g->ray.step.y = 1;
+		g->ray.ray_len.y = ((float)(g->ray.check.y + 1) - g->ray.ray_start.y) * g->ray.ray_unit.y;
 	}
-	wall = 0;
-	door = 0;
-	dist = 0;
-	while (wall == 0 && door == 0)
+	g->ray.wall = 0;
+	g->ray.door = 0;
+	g->ray.dist = 0;
+	while (g->ray.wall == 0 && g->ray.door == 0)
 	{
-		if (ray_len.x < ray_len.y)
+		if (g->ray.ray_len.x < g->ray.ray_len.y)
 		{
-			check.x += step.x;
-			dist = ray_len.x;
-			ray_len.x += ray_unit.x;
+			g->ray.check.x += g->ray.step.x;
+			g->ray.dist = g->ray.ray_len.x;
+			g->ray.ray_len.x += g->ray.ray_unit.x;
 		}
 		else
 		{
-			check.y += step.y;
-			dist = ray_len.y;
-			ray_len.y += ray_unit.y;
+			g->ray.check.y += g->ray.step.y;
+			g->ray.dist = g->ray.ray_len.y;
+			g->ray.ray_len.y += g->ray.ray_unit.y;
 		}
-		if (g->map.map[check.y][check.x] == '1')
-			wall = 1;
-		else if (g->map.map[check.y][check.x] == '-')
-			door = 1;
+		if (g->map.map[g->ray.check.y][g->ray.check.x] == '1')
+			g->ray.wall = 1;
+		else if (g->map.map[g->ray.check.y][g->ray.check.x] == '-')
+			g->ray.door = 1;
 	}
+	g->ray.dist *= cos(angle - g->player.angle_view);
 	double	cam_dist;
 	double	wall_ratio;
 	double	wall_size;
 	cam_dist = (960) / tan(M_PI / 6);
-	wall_ratio = cam_dist / dist;
+	wall_ratio = cam_dist / g->ray.dist;
 	// wall_size = wall_ratio * WINDOW_Y;
 	if (angle - g->player.angle_view > 0)
 		wall_size = wall_ratio * cos(0.5235);
@@ -90,9 +85,9 @@ void	raycasting(t_game *g, float angle)
 	{
 		if (g->size.y >= top_wall && g->size.y <= bottom_wall)
 		{
-			if (wall)
+			if (g->ray.wall)
 				my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREY);
-			else if (door)
+			else if (g->ray.door)
 				my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREEN);
 		}
 		g->size.y++;
