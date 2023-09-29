6af72794da887f119d6f2ce32f5f15cb58ec5914
Thea Wang <twang@z4r8p1.42lyon.fr>
2023-09-29 13:35:25 +0200

    resolve conflicts

--- sources/bonus/displays/game/raycasting/raycasting.c
+++ sources/bonus/displays/game/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: twang <twang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/04 13:05:31 by aascedu           #+#    #+#             */
-/*   Updated: 2023/09/29 13:30:22 by twang            ###   ########.fr       */
+/*   Updated: 2023/09/29 13:33:48 by twang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,25 +82,7 @@ static void	_init_ray(t_game *g)
 	}
 }
 
-void	find_dir_wall(t_game *g, int check)
-{
-	if (check == 1) // Vertical
-	{
-		if (g->ray.check.x < g->player.pos.x)
-			g->ray.wall_dir = 1;
-		else if (g->ray.check.x > g->player.pos.x)
-			g->ray.wall_dir = 3;
-	}
-	else
-	{
-		if (g->ray.check.y < g->player.pos.y)
-			g->ray.wall_dir = 2;
-		else if (g->ray.check.y > g->player.pos.y)
-			g->ray.wall_dir = 4;
-	}
-}
-
-void	find_dist(t_game *g, float angle)
+static void	_find_dist(t_game *g, float angle)
 {
 	while (g->ray.wall == 0 && g->ray.door == 0)
 	{
@@ -124,39 +106,4 @@ void	find_dist(t_game *g, float angle)
 			g->ray.door = 1;
 	}
 	g->ray.dist *= cos(angle - g->player.angle_view);
-}
-
-void	draw_textures(t_game *g)
-{
-	if (g->ray.wall && g->ray.wall_dir == 1)
-		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREY);
-	else if (g->ray.wall && g->ray.wall_dir == 2)
-		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_BLUE);
-	else if (g->ray.wall && g->ray.wall_dir == 3)
-		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_WHITE);
-	else if (g->ray.wall && g->ray.wall_dir == 4)
-		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_RED);
-	else if (g->ray.door)
-		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREEN);
-}
-
-void	raycasting(t_game *g, float angle)
-{
-	init_var(g, angle);
-	init_ray(g);
-	find_dist(g, angle);
-	g->ray.wall_ratio = g->ray.cam_dist / g->ray.dist;
-	if (angle - g->player.angle_view > 0)
-		g->ray.wall_size = g->ray.wall_ratio * cos(0.5235);
-	else
-		g->ray.wall_size = g->ray.wall_ratio * cos(-0.5235);
-	g->ray.top_wall = (540) - (g->ray.wall_size / 2);
-	g->ray.bottom_wall = (540) + (g->ray.wall_size / 2);
-	while (g->size.y <= WINDOW_Y - 1 && g->size.y >= 0
-		&& g->size.x <= WINDOW_X - 1 && g->size.x >= 0)
-	{
-		if (g->size.y >= g->ray.top_wall && g->size.y <= g->ray.bottom_wall)
-			draw_textures(g);
-		g->size.y++;
-	}
 }
