ae55f7aeff7fadf9daf5c073b92d37c8e5c874da
Arthur <arthurascedusnkrs@gmail.com>
2023-10-03 16:54:17 +0200

    Ca glisse un peu

--- sources/bonus/displays/game/texturing/texturing.c
+++ sources/bonus/displays/game/texturing/texturing.c
@@ -51,11 +51,9 @@ void	find_dir_wall(t_game *g, int check)
 static void	_texturing_vertical(t_game *g)
 {
 	double		wall_x;
-	double		ratio;
 	t_vector	texture;
 	unsigned int	color;
 
-	ratio = g->textures.walls[north_texture].width / g->ray.wall_size;
 	wall_x = g->ray.ray_start.y + g->ray.dist * g->ray.ray_dir.y;
 	wall_x = wall_x - floor(wall_x);
 	texture.x = wall_x * g->textures.walls[north_texture].width;
@@ -71,14 +69,15 @@ static void	_texturing_horizontal(t_game *g)
 {
 	double		wall_x;
 	t_vector	texture;
+	unsigned int	color;
 
-	wall_x = g->ray.ray_start.y + g->ray.dist * g->ray.ray_dir.y;
+	wall_x = g->ray.ray_start.x + g->ray.dist * g->ray.ray_dir.x;
 	wall_x = wall_x - floor(wall_x);
 	texture.x = wall_x * g->textures.walls[north_texture].width;
 	texture.x = g->textures.walls[north_texture].width - texture.x - 1;
 	texture.y = (g->size.y * 2 - 1080 + g->ray.wall_size) * (g->textures.walls[north_texture].width / 2) / g->ray.wall_size;
-	// printf("x=%d y=%d\n", texture.x, texture.y);
-	// my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, );
+	color = my_mlx_pixel_get(&g->textures.walls[north_texture], texture.x, texture.y);
+	my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, color);
 }
 
 void	draw_textures(t_game *g)
