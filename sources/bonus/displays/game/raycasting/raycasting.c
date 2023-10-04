7a4cc5d99caab61ddf06b37343c7bd4f8920a78e
Arthur <arthurascedusnkrs@gmail.com>
2023-10-04 16:09:40 +0200

    Texture Biatch

--- sources/bonus/displays/game/raycasting/raycasting.c
+++ sources/bonus/displays/game/raycasting/raycasting.c
@@ -21,7 +21,7 @@ static void	_find_dist(t_game *g, float angle);
 
 /*----------------------------------------------------------------------------*/
 
-void	raycasting(t_game *g, float angle)
+void	raycasting(t_game *g, double angle)
 {
 	_init_var(g, angle);
 	_init_ray(g);
@@ -38,7 +38,7 @@ void	raycasting(t_game *g, float angle)
 	{
 		if (g->size.y >= g->ray.top_wall && g->size.y <= g->ray.bottom_wall)
 		{
-			draw_textures(g, 0.333);
+			draw_textures(g);
 		}
 		g->size.y++;
 	}
@@ -86,7 +86,7 @@ static void	_init_ray(t_game *g)
 
 static void	_find_dist(t_game *g, float angle)
 {
-	while (g->ray.wall == 0 && g->ray.door == 0 && g->ray.dist < 10)
+	while (g->ray.wall == 0 && g->ray.door == 0)
 	{
 		if (g->ray.ray_len.x < g->ray.ray_len.y)
 		{
@@ -107,6 +107,8 @@ static void	_find_dist(t_game *g, float angle)
 		else if (g->map.map[g->ray.check.y][g->ray.check.x] == '-')
 			g->ray.door = 1;
 	}
-	if (g->ray.dist < 10)
-		g->ray.dist = g->ray.dist * cos(angle - g->player.angle_view);
+	g->ray.intersection.x = g->ray.dist * g->ray.ray_dir.x + g->player.posf.x;
+	g->ray.intersection.y = g->ray.dist * g->ray.ray_dir.y + g->player.posf.y;
+	g->ray.dist = g->ray.dist * cos(fabs(angle - g->player.angle_view));
+
 }
--- sources/bonus/displays/game/texturing/texturing.c
+++ sources/bonus/displays/game/texturing/texturing.c
@@ -38,7 +38,7 @@ void	find_dir_wall(t_game *g, int check)
 	}
 }
 
-// void	draw_textures(t_game *g)
+// void	draw_textures(t_game *g, double dist)
 // {
 // 	if (g->ray.wall && g->ray.wall_dir == west)
 // 		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREY);
@@ -52,17 +52,16 @@ void	find_dir_wall(t_game *g, int check)
 // 		my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, H_GREEN);
 // }
 
-static void	_texturing_vertical(t_game *g, double dist)
+static void	_texturing_vertical(t_game *g)
 {
 	double		wall_x;
 	t_vector	texture;
 	unsigned int	color;
 
-	wall_x = g->ray.dist * g->ray.ray_dir.y + g->player.posf.y;
-	// printf("%f * %f = %f, ")
+	wall_x = g->ray.intersection.y;
 	wall_x = wall_x - floor(wall_x);
 	texture.x = wall_x * g->textures.walls[north_texture].width;
-	texture.y = (g->size.y * 2 - 1080 + g->ray.wall_size) * (g->textures.walls[north_texture].width / 2) / g->ray.wall_size;
+ 	texture.y = (g->size.y * 2 - 1080 + g->ray.wall_size) * (g->textures.walls[north_texture].width / 2) / g->ray.wall_size;
 	color = my_mlx_pixel_get(&g->textures.walls[north_texture], texture.x, texture.y);
 	my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, color);
 }
@@ -75,22 +74,18 @@ static void	_texturing_horizontal(t_game *g)
 	t_vector	texture;
 	unsigned int	color;
 
-	wall_x = g->ray.dist * g->ray.ray_dir.x + g->player.posf.x;
+	wall_x = g->ray.intersection.x;
 	wall_x = wall_x - floor(wall_x);
 	texture.x = wall_x * g->textures.walls[north_texture].width;
-	if (g->ray.ray_dir.y < 0)
-		texture.x = g->textures.walls[north_texture].width - texture.x - 1;
-	else
-		texture.x = texture.x - 1;
 	texture.y = (g->size.y * 2 - 1080 + g->ray.wall_size) * (g->textures.walls[north_texture].width / 2) / g->ray.wall_size;
 	color = my_mlx_pixel_get(&g->textures.walls[north_texture], texture.x, texture.y);
 	my_mlx_pixel_put(&g->draw, g->size.x, g->size.y, color);
 }
 
-void	draw_textures(t_game *g, double dist)
+void	draw_textures(t_game *g)
 {
 	if (g->ray.wall_dir == north || g->ray.wall_dir == south)
 		_texturing_horizontal(g);
 	else
-		_texturing_vertical(g, dist);
+		_texturing_vertical(g);
 }
