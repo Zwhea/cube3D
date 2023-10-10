dc501d75ff8307372b4f8b30682cfdc4ed124361
Arthur <arthurascedusnkrs@gmail.com>
2023-10-10 14:42:43 +0200

    Animation hands moving when touch movement is pressed

--- sources/bonus/displays/draw/draw_hands.c
+++ sources/bonus/displays/draw/draw_hands.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw_hands.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aascedu <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 13:53:23 by aascedu           #+#    #+#             */
+/*   Updated: 2023/10/10 13:53:24 by aascedu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "cub3D_thea.h"
 #include "cub3D_arthur.h"
 
@@ -8,15 +20,18 @@ void	draw_hands(t_game *g)
 	unsigned int	color;
 
 	x_text = 0;
-	while (x_text < g->animations.player[0].width)
+	while (x_text < g->sprites.player[0].width * 4)
 	{
 		y_text = 0;
-		while (y_text < g->animations.player[0].width)
+		while (y_text < g->sprites.player[0].width * 4)
 		{
-			color = my_mlx_pixel_get(&g->animations.player[0], \
-				x_text, y_text);
+			color = my_mlx_pix_get(&g->sprites.player[0], \
+				x_text / 4, y_text / 4);
 			if (color != H_GREEN)
-				my_mlx_pixel_put(&g->draw, 895 + y_text, 1015 + x_text, color);
+				my_mlx_pixel_put(&g->draw, ((1920 * 0.5) - 4
+						* g->sprites.player[0].width - 1) + y_text,
+					(1080 - 4 * g->sprites.player[0].width + 15) + x_text
+					+ (int)(cos(g->player.hand_move) * 10), color);
 			y_text++;
 		}
 		x_text++;
