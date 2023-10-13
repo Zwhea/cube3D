3840cb87f0e99022024882a01a364fe29248ce70
Arthur <arthurascedusnkrs@gmail.com>
2023-10-13 11:01:10 +0200

    Fix Shitty Check Doors (That I did wrong ok Thea le S)

--- sources/bonus/displays/motions/doors.c
+++ sources/bonus/displays/motions/doors.c
@@ -32,59 +32,59 @@ int	open_door(t_game *g)
 
 static void	_init_raycasting(t_game *g)
 {
-	g->ray.ray_unit.x = sqrt(1 + pow(tan(g->player.angle_view), 2));
-	g->ray.ray_unit.y = sqrt(1 + pow(1 / tan(g->player.angle_view), 2));
-	g->ray.ray_dir.x = cos(g->player.angle_view);
-	g->ray.ray_dir.y = sin(g->player.angle_view);
-	g->ray.ray_start = g->player.posf;
-	g->ray.check = g->player.pos;
-	g->ray.door = 0;
-	g->ray.dist = 0;
+	g->door.ray_unit.x = sqrt(1 + pow(tan(g->player.angle_view), 2));
+	g->door.ray_unit.y = sqrt(1 + pow(1 / tan(g->player.angle_view), 2));
+	g->door.ray_dir.x = cos(g->player.angle_view);
+	g->door.ray_dir.y = sin(g->player.angle_view);
+	g->door.ray_start = g->player.posf;
+	g->door.check = g->player.pos;
+	g->door.door = 0;
+	g->door.dist = 0;
 }
 
 static void	_init_ray_door(t_game *g)
 {
-	if (g->ray.ray_dir.x < 0)
+	if (g->door.ray_dir.x < 0)
 	{
-		g->ray.step.x = -1;
-		g->ray.ray_len.x = (g->ray.ray_start.x - (float)g->ray.check.x)
-			* g->ray.ray_unit.x;
+		g->door.step.x = -1;
+		g->door.ray_len.x = (g->door.ray_start.x - (float)g->door.check.x)
+			* g->door.ray_unit.x;
 	}
 	else
 	{
-		g->ray.step.x = 1;
-		g->ray.ray_len.x = ((float)(g->ray.check.x + 1) - g->ray.ray_start.x)
-			* g->ray.ray_unit.x;
+		g->door.step.x = 1;
+		g->door.ray_len.x = ((float)(g->door.check.x + 1) - g->door.ray_start.x)
+			* g->door.ray_unit.x;
 	}
-	if (g->ray.ray_dir.y < 0)
+	if (g->door.ray_dir.y < 0)
 	{
-		g->ray.step.y = -1;
-		g->ray.ray_len.y = (g->ray.ray_start.y - (float)g->ray.check.y)
-			* g->ray.ray_unit.y;
+		g->door.step.y = -1;
+		g->door.ray_len.y = (g->door.ray_start.y - (float)g->door.check.y)
+			* g->door.ray_unit.y;
 	}
 	else
 	{
-		g->ray.step.y = 1;
-		g->ray.ray_len.y = ((float)(g->ray.check.y + 1) - g->ray.ray_start.y)
-			* g->ray.ray_unit.y;
+		g->door.step.y = 1;
+		g->door.ray_len.y = ((float)(g->door.check.y + 1) - g->door.ray_start.y)
+			* g->door.ray_unit.y;
 	}
 }
 
 static void	_raycasting_door(t_game *g)
 {
-	while (g->ray.dist < 3)
+	while (g->door.dist < 3)
 	{
-		if (g->ray.ray_len.x < g->ray.ray_len.y)
+		if (g->door.ray_len.x < g->door.ray_len.y)
 		{
-			g->ray.check.x += g->ray.step.x;
-			g->ray.dist = g->ray.ray_len.x;
-			g->ray.ray_len.x += g->ray.ray_unit.x;
+			g->door.check.x += g->door.step.x;
+			g->door.dist = g->door.ray_len.x;
+			g->door.ray_len.x += g->door.ray_unit.x;
 		}
 		else
 		{
-			g->ray.check.y += g->ray.step.y;
-			g->ray.dist = g->ray.ray_len.y;
-			g->ray.ray_len.y += g->ray.ray_unit.y;
+			g->door.check.y += g->door.step.y;
+			g->door.dist = g->door.ray_len.y;
+			g->door.ray_len.y += g->door.ray_unit.y;
 		}
 		if (_check_door(g))
 		{
@@ -96,18 +96,18 @@ static void	_raycasting_door(t_game *g)
 
 static int	_check_door(t_game *g)
 {
-	if (g->ray.dist < 3 && g->ray.check.x >= 0 && g->ray.check.y >= 0 \
-			&& g->map.map[g->ray.check.y] \
-			&& g->ray.check.x < g->map.line_len[g->ray.check.y] \
-			&& g->map.map[g->ray.check.y][g->ray.check.x] == door)
+	if (g->door.dist < 3 && g->door.check.x >= 0 && g->door.check.y >= 0 \
+			&& g->map.map[g->door.check.y] \
+			&& g->door.check.x < g->map.line_len[g->door.check.y] \
+			&& g->map.map[g->door.check.y][g->door.check.x] == door)
 	{
 		g->sprites.is_open = false;
 		return (1);
 	}
-	else if (g->ray.dist < 3 && g->ray.check.x >= 0 && g->ray.check.y >= 0 \
-			&& g->map.map[g->ray.check.y] \
-			&& g->ray.check.x < g->map.line_len[g->ray.check.y] \
-			&& g->map.map[g->ray.check.y][g->ray.check.x] == o_door)
+	else if (g->door.dist < 3 && g->door.check.x >= 0 && g->door.check.y >= 0 \
+			&& g->map.map[g->door.check.y] \
+			&& g->door.check.x < g->map.line_len[g->door.check.y] \
+			&& g->map.map[g->door.check.y][g->door.check.x] == o_door)
 	{
 		g->sprites.is_open = true;
 		return (1);
